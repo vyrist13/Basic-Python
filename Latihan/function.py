9f3c69851f36c33cc96e28224ca00144951a9502
vyrist13 <toma.saputra@gmail.com>
2021-06-13 13:42:50 +0700

    commit

--- Latihan/function.py
+++ Latihan/function.py
@@ -1,15 +1,15 @@
-def function():
-    print("Saya makan")
-    print("Saya minum")
-    print("Saya tidur")
-function()
+#def function():
+    #print("Saya makan")
+    #print("Saya minum")
+    #print("Saya tidur")
+#function()
 
-def nama(name):
-    print("Nama saya",name)
-nama("Ali")
-nama("Badru")
+#def nama(name):
+    #print("Nama saya",name)
+#nama("Ali")
+#nama("Badru")
 
-def method(name="None"):
+def method(name="Tidak Tahu"):
     print("Nama saya "+name)
 method()
 
@@ -17,3 +17,4 @@ def fungsi(nama, tinggi, umur):
     print("Nama saya",nama)
     print("Tinggi saya",tinggi)
     print("Umur saya",umur)
+fungsi(umur=36)
