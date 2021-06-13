e614a86ea348fa5277e2b0b52dc7500d2a973a81
vyrist13 <toma.saputra@gmail.com>
2021-06-13 14:20:53 +0700

    commit

---
+++ Latihan/dictionary(1).py
@@ -0,0 +1,18 @@
+thisdict={
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+print(thisdict)
+
+x=thisdict["model"]
+
+thisdict={
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+thisdict["year"]=2018
+
+for x in thisdict:
+    print (x)
--- Latihan/dictionary.py
+++ Latihan/dictionary.py
@@ -1,30 +1,34 @@
 d={
-    "Nama":"Thoma Saputra",
-    "Umur":36,
-    "Tinggi":172,
+    "nama":"Thoma Saputra",
+    "umur":36,
+    "tinggi":172,
     #key   #value
 }
-print(d["Nama"])
+print(d["nama"])
 print(d)
-d["Nama"]="Thoma"
+d["nama"]="Thoma"
 print(d)
 
-for x in d:
-    print(d)
+#for x in d:
+    #print(d)
 
 
 d=[]
 
 for i in range(1):
     nama=input("Masukkan nama anda: ")
-    umur=int(input"(Masukkan umur anda: "))
-    tinggi=float(input"(Masukkan tinggi anda: "))
+    umur=int(input("Masukkan umur anda: "))
+    tinggi=float(input("Masukkan tinggi anda: "))
     data={
-        "Nama":nama
-        "Umur":umur
-        "Tinggi":tinggi
+        "nama":nama,
+        "umur":umur,
+        "tinggi":tinggi
     }
     d.append(data)
 for i in d:
+    print("Nama Pelanggan: ",i["nama"])
+    print("Umur Pelanggan: ",i["umur"])
+    print("Tinggi Pelanggan: ",i["tinggi"])
+
     
 
