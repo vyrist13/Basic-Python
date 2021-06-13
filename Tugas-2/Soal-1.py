e5236452e9c8840e66302c0e4eac4876008f7a8c
vyrist13 <toma.saputra@gmail.com>
2021-06-13 17:32:49 +0700

    commit

--- Tugas-2/Soal-1.py
+++ Tugas-2/Soal-1.py
@@ -1,10 +1,9 @@
 def greeting():
-    print("Selamat datang!")
+    print("Selamat Datang!")
     print("---Menu---")
     print("1. Daftar Kontak")
     print("2. Tambah Kontak")
     print("3. Keluar")
-
 kontak={
     "Farhan":"08123456789",
     "Joko":"08987654321",
@@ -12,22 +11,27 @@ kontak={
 pilih_menu=0
 greeting()
 while pilih_menu !=3:
-    pilih_menu=int(input("Pilih menu:"))
+    print()
+    pilih_menu=int(input("Pilih Menu: "))
     if pilih_menu==1:
+        print()
         print("---Daftar Kontak---")
         for x in kontak.keys():
-            print("Nama:",x)
-            print("No. Telepon:",kontak[x])
-        print()
+            print("Nama:",x,"\tNo. Telepon:",kontak[x])
     elif pilih_menu==2:
-        nama=input("Nama:")
-        telp=input("No. Telepon:")
+        print()
+        print("---Pastikan Nama yang Akan Ditambahkan Berbeda dengan yang Sudah Ada di Kontak---")
+        nama=input("Nama: ")
+        telp=input("No. Telepon: ")
         kontak[nama]=telp
+        print()
         print("---Kontak Berhasil Ditambahkan---")
+        print("---Silakan Pilih Menu 1 Untuk Cek Kontak Tambahan---")
     elif pilih_menu==3:
         print("---Program Selesai, Sampai Jumpa!---")
+        print()
     else:
-        print("---Menu Tidak Tersedia---")
+        print("---Menu Tidak Tersedia. Silakan Pilih Angka 1 sampai 3---")
 
 
     
