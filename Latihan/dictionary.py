e393097f8fcfa932f51c5c802a59aa326c424373
vyrist13 <toma.saputra@gmail.com>
2021-06-13 13:34:26 +0700

    commit

---
+++ Latihan/dictionary.py
@@ -0,0 +1,30 @@
+d={
+    "Nama":"Thoma Saputra",
+    "Umur":36,
+    "Tinggi":172,
+    #key   #value
+}
+print(d["Nama"])
+print(d)
+d["Nama"]="Thoma"
+print(d)
+
+for x in d:
+    print(d)
+
+
+d=[]
+
+for i in range(1):
+    nama=input("Masukkan nama anda: ")
+    umur=int(input"(Masukkan umur anda: "))
+    tinggi=float(input"(Masukkan tinggi anda: "))
+    data={
+        "Nama":nama
+        "Umur":umur
+        "Tinggi":tinggi
+    }
+    d.append(data)
+for i in d:
+    
+
---
+++ Latihan/function.py
@@ -0,0 +1,19 @@
+def function():
+    print("Saya makan")
+    print("Saya minum")
+    print("Saya tidur")
+function()
+
+def nama(name):
+    print("Nama saya",name)
+nama("Ali")
+nama("Badru")
+
+def method(name="None"):
+    print("Nama saya "+name)
+method()
+
+def fungsi(nama, tinggi, umur):
+    print("Nama saya",nama)
+    print("Tinggi saya",tinggi)
+    print("Umur saya",umur)
