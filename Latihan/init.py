3e8868a2904d069dc80a9fa4c3b2b784e697bca9
vyrist13 <toma.saputra@gmail.com>
2021-06-20 13:20:05 +0700

    commit

---
+++ Latihan/init.py
@@ -0,0 +1,9 @@
+class harimau:
+    def __init__(self, kaki, gigi, makanan):
+        self.kaki=kaki
+        self.gigi=gigi
+        self.makanan=makanan
+
+animal=harimau(4,"Punya Taring","Daging")
+print(animal.kaki)
+print(animal.gigi)
