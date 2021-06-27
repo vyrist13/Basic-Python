548545fcd7ee1b0f365480935f4b2bd969b40d0f
vyrist13 <toma.saputra@gmail.com>
2021-06-27 15:36:28 +0700

    commit

--- Final-Project/Final_Project.py
+++ Final-Project/Final_Project.py
@@ -49,3 +49,5 @@ with smtplib.SMTP_SSL("smtp.gmail.com",465,context=context) as server:
     server.sendmail(
         sender_email,receiver_email,message.as_string()
     )
+sess
+print("Mail Sent")
